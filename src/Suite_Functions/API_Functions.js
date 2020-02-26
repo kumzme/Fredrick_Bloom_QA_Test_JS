de3fa272267c08b3c192269ee8a8f70bdb8aa024
kumzme <kumzme@github.com>
2020-02-26 09:56:09 -0500

    Some Json Advanced and Selenium Common functions

--- src/Suite_Functions/API_Functions.js
+++ src/Suite_Functions/API_Functions.js
@@ -1,18 +1,16 @@
 
 
 export async function API_Get(Url_Reference, Body_Text, done) {
-/*
-    let aa = await request.get(__Test_Data[Url_Reference]);let aa01 = await request.get(__Test_Data[Url_Reference]).on('response', function(response) {console.log(response.statusCode);console.log(response.headers['content-type']);console.log(JSON.stringify(response));Object.assign(body_Return, response);});
-    let aa_01 = await aa.on('response', function(response) {console.log(response.statusCode);console.log(response.headers['content-type']);console.log(JSON.stringify(response));Object.assign(body_Return, response);});
-*/
-
     const request = await require('request');
     const request_01 = await request.get(__Test_Data[Url_Reference]).on('response',
         (response) => {
             return  response;
             done();
         },
-    );
+    ).on('error', (error)=>{
+        console.log("Api Call Error:" + error);
+        throw new Error("Api Call Error:" + error);
+    });
     return request_01;
     done();
 }
--- src/Suite_Functions/Generic.js
+++ src/Suite_Functions/Generic.js
@@ -3,38 +3,13 @@ async function Delay_For(This_Number) {
         continue;
     }
 };
-
-export async function findAllByKey(Sent_Object, Find_The_Key) {
-    return Object.entries(Sent_Object)
-        .reduce((Every_Object_In, [key, value]) => {
-                if ((key === Find_The_Key)) {
-                    Every_Object_In.concat(value);
-                } else {
-                    if (typeof value === 'object') {
-                        Every_Object_In.concat(findAllByKey(value, Find_The_Key))
-                    } else {
-                        Every_Object_In
-                    }
-                }
-            }
-            , [])
-}
-export async function findAllByKey2(obj, keyToFind) {
-    return Object.entries(obj)
-        .reduce((acc, [key, value]) => (key === keyToFind)
-            ? acc.concat(value)
-            : (typeof value === 'object')
-                ? acc.concat(findAllByKey(value, keyToFind))
-                : acc
-            , [])
-}
 // Get all instances of specified property deep within supplied object
-export async function Deep_Search_Json_Recursive(Search_Object, Search_this, Get_Multiple_Values_Array) {
+export async function Deep_Search_Json_Recursive(Search_Object, Search_this, Final_Return_Multiple_Values_Array) {
     if (Search_Object instanceof Array) {
         for (let i = 0; i < Search_Object.length; i++) {
             var Indv_Element = Search_Object[i];
             if (Indv_Element instanceof Object || Indv_Element instanceof Array) {
-                Get_Multiple_Values_Array =   await Get_Multiple_Values_Array.concat(await Deep_Search_Json_Recursive(Indv_Element, Search_this, Get_Multiple_Values_Array));
+                Final_Return_Multiple_Values_Array =   await Final_Return_Multiple_Values_Array.concat(await Deep_Search_Json_Recursive(Indv_Element, Search_this, Final_Return_Multiple_Values_Array));
             }
         }
     } else {
@@ -45,14 +20,14 @@ export async function Deep_Search_Json_Recursive(Search_Object, Search_this, Get
                     return Search_Object[Single_Key];
                 }
                 if (Current_Val instanceof Object || Current_Val instanceof Array) {
-                    Get_Multiple_Values_Array =  await Get_Multiple_Values_Array.concat( await Deep_Search_Json_Recursive(Current_Val, Search_this, Get_Multiple_Values_Array));
+                    Final_Return_Multiple_Values_Array =  await Final_Return_Multiple_Values_Array.concat( await Deep_Search_Json_Recursive(Current_Val, Search_this, Final_Return_Multiple_Values_Array));
                 }
             }
         } else {
             throw new Error('Check passed Type to Deep_Search_Json_Recursive' + typeof Search_Object);
         }
     }
-    return Get_Multiple_Values_Array;
+    return Final_Return_Multiple_Values_Array;
 }
 
 module.exports = {
